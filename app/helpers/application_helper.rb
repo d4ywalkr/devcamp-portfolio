96c9cf7817e63eaa5468b7f81a6758a334ac43c8
Renzo <renzoscheffer@gmail.com>
2018-06-07 18:48:40 +0200

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,11 +51,22 @@ module ApplicationHelper
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
-    
+
     nav_links.html_safe
   end
 
   def active? path
     "active" if current_page? path
+  end
+
+  def alerts
+    alert = (flash[:error] || flash[:alert] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, :title => "Renzel is saying...", sticky: false)
   end
 end
